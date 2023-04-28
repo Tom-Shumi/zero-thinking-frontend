39b5f5dc8df0dcbdc5190ad4d3be059ac2ffe439
Tom-Shumi <tshumiya281@gmail.com>
2023-04-28 22:33:52 +0900

    add: タイマー

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,6 @@ export default defineNuxtConfig({
   postcss: {
     plugins: { tailwindcss: {} }
   },
-  css: ['~/assets/css/tailwind.css'],
+  css: ['~/assets/css/tailwind.css', 'easymde/dist/easymde.min.css'],
   modules: ['@nuxtjs/tailwindcss']
 })
--- pages/thinking/index.vue
+++ pages/thinking/index.vue
@@ -1,37 +1,90 @@
 <template>
-  <div class="text-center"><h1 class="text-7xl font-bold m-5">60</h1></div>
+  <div class="text-center">
+    <h1 class="text-7xl font-bold m-5">{{ remainingTime }}</h1>
+  </div>
   <div class="mt-5 ml-20">
-    <div><label for="title">テーマ</label></div>
+    <div><label for="theme">テーマ</label></div>
     <div class="mb-8">
-      <input type="text" id="title" name="title" class="border w-4/5" />
+      <input type="text" v-model="theme" class="border w-4/5" />
     </div>
   </div>
   <div class="mb-5 text-center">
-    <PrimaryButton label="START" :on-click="handleStart" />
+    <PrimaryButton
+      class="mx-5"
+      label="START"
+      :on-click="handleStart"
+      v-show="!isTimerOn"
+    />
+    <SecondaryButton
+      class="mx-5"
+      label="STOP"
+      :on-click="handleStop"
+      v-show="isTimerOn"
+    />
+    <GrayButton class="mx-5" label="RESET" :on-click="handleReset" />
   </div>
   <div class="ml-8 mr-8">
     <hr class="w-full" />
   </div>
   <div class="mt-5 ml-20">
-    <label for="thinking">深掘り</label>
+    <label for="thinkingTree">深掘り</label>
     <div class="mb-8">
-      <input type="text" name="thinking" class="border w-3/5" />
+      <textarea class="border w-4/5"></textarea>
+      <!-- <vue-easymde v-model="thinkingTree" /> -->
     </div>
   </div>
 </template>
 
 <script lang="ts">
   import PrimaryButton from '~/components/atoms/buttons/PrimaryButton.vue'
+  import SecondaryButton from '~/components/atoms/buttons/SecondaryButton.vue'
+  import GrayButton from '~/components/atoms/buttons/GrayButton.vue'
+  // import VueEasymde from 'vue-easymde'
+
+  const THINKING_TIME = 5
 
   export default defineComponent({
-    components: { PrimaryButton },
-    setup() {
-      const handleStart = () => {
-        console.log('start')
-      }
-
+    components: { PrimaryButton, SecondaryButton, GrayButton },
+    data() {
       return {
-        handleStart
+        remainingTime: THINKING_TIME,
+        isTimerOn: false,
+        timerObj: undefined as NodeJS.Timer | undefined,
+        theme: '',
+        thinkingTree: ''
+      }
+    },
+    methods: {
+      handleStart() {
+        if (this.theme == '') {
+          alert('テーマを入力してください')
+          return
+        }
+        const self = this
+        this.remainingTime = THINKING_TIME
+        this.timerObj = setInterval(function () {
+          self.countDown()
+        }, 1000)
+        this.isTimerOn = true
+      },
+      handleStop() {
+        clearInterval(this.timerObj)
+        this.isTimerOn = false
+      },
+      handleReset() {
+        this.handleStop()
+        this.remainingTime = THINKING_TIME
+      },
+      countDown() {
+        this.remainingTime = this.remainingTime - 1
+      }
+    },
+    watch: {
+      remainingTime(newVal, _) {
+        if (newVal == 0) {
+          this.handleStop()
+          alert('時間切れです！')
+        }
       }
     }
   })
